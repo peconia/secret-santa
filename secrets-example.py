0a02ce239299ad19b042bd60501c105ea2377e0e
PAIVI-MINI-PC\paivi <peconia@gmail.com>
2024-09-22 23:34:04 +0100

    remove unused example

--- secrets-example.py
+++ secrets-example.py
@@ -1,5 +1,4 @@
 # note, sheet should be public
 SHEET_URL = 'https://docs.google.com/spreadsheets/d/abcdefg12345/edit'
-API_KEY = 'your-google-api-key'
 SENDER_EMAIL = 'your-gmail-address'
 SENDER_PASSWORD = 'your-gmail-APP-password'  # must be an app password, not your regular one
