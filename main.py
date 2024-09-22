8fa2c03f357c1431d2bd2db7cbff48465716fa63
PAIVI-MINI-PC\paivi <peconia@gmail.com>
2024-09-22 19:10:41 +0100

    started secret santa

---
+++ main.py
@@ -0,0 +1,82 @@
+import csv
+import random
+import requests
+import secrets
+from messenger import process_santa_assignment_emails
+
+MAX_ATTEMPTS = 200
+EMAIL_SUBJECT = '**TEST** Your Secret Santa Assignment'
+
+
+def read_google_sheet(sheet_url):
+    response = requests.get(sheet_url)
+    data = response.content.decode('utf-8')
+    reader = csv.reader(data.splitlines())
+    next(reader)  # remove first row with column names
+    return list(reader)
+
+
+def participant_list(data):
+    participants = []
+    for row in data:
+        participants.append({'name': row[0], 'email': row[1], 'household': row[2]})
+    return participants
+
+
+def naive_allocate_santa(participants):
+    targets = participants.copy()
+    allocated = []
+
+    random.shuffle(participants)
+    random.shuffle(targets)
+
+    for participant in participants:
+        target = targets.pop()
+        attempts = 0
+        while target['email'] == participant['email'] and attempts < len(participants):
+            targets.insert(0, target)
+
+            target = targets.pop()
+            attempts += 1
+        allocated.append([participant, target])
+
+    return allocated
+
+
+def check_validity(allocated):
+    for pair in allocated:
+        if pair[0]['email'] == pair[1]['email']:
+            # print('Error: ' + pair[0]['name'] + ' is assigned to themselves')
+            return False
+        if pair[0]['household'] == pair[1]['household']:
+            # print('Error: ' + pair[0]['name'] + ' is assigned to someone in their household')
+            return False
+    print('All assignments are valid')
+    return True
+
+
+def get_valid_santa_assignments(participants):
+    allocated = naive_allocate_santa(participants)
+    attempts = 1
+    while not check_validity(allocated) and attempts < MAX_ATTEMPTS:
+        allocated = naive_allocate_santa(participants)
+        attempts += 1
+
+    if attempts == MAX_ATTEMPTS and not check_validity(allocated):
+        raise Exception('Failed to find valid assignments after {} attempts'.format(MAX_ATTEMPTS))
+
+    print('Found valid assignments after ' + str(attempts) + ' attempts')
+    return allocated
+
+
+def santa():
+    sheet_url = secrets.SHEET_URL.split('/edit')[0] + '/export?format=csv&usp=sharing'
+
+    data = read_google_sheet(sheet_url)
+    participants = participant_list(data)
+    allocated = get_valid_santa_assignments(participants)
+    process_santa_assignment_emails(allocated, participants, EMAIL_SUBJECT)
+
+
+if __name__ == '__main__':
+    santa()
---
+++ messenger.py
@@ -0,0 +1,78 @@
+import secrets
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def hydrate_message(message, target, participants_list):
+    # replace {{name}} with the name of the target
+    message = message.replace('{{gift_recipient_name}}', target['name'])
+    message = message.replace('{{participants}}', participants_list)
+    return message
+
+
+def format_html_participants(participants):
+    participants_list = ''
+    for participant in participants:
+        participants_list += '<li>' + participant['name'] + '</li>'
+    return participants_list
+
+
+def html_email_template(recipient, participants_html):
+    # import html template from santa_email.html
+    with open('santa_email.html', 'r', encoding='utf-8') as file:
+        html_message = file.read()
+        message = hydrate_message(html_message, recipient, participants_html)
+        return message
+
+
+def process_santa_assignment_emails(allocated_pairs, participants, subject):
+    html_participants = format_html_participants(participants)
+    emails_to_send = []
+
+    for pair in allocated_pairs:
+        email_recipient = pair[0]
+        gift_recipient = pair[1]
+
+        # print('{} is sending a gift to {}'.format(email_recipient['name'], gift_recipient['name']))
+        message_html = html_email_template(gift_recipient, html_participants)
+
+        emails_to_send.append({'recipient': email_recipient['email'], 'message_html': message_html})
+
+    for email in emails_to_send:
+        send_email(email['recipient'], email['message_html'], subject)
+
+
+def send_gmail_email(recipient_email, subject, html_body):
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+
+    msg = MIMEMultipart()
+    msg['From'] = secrets.SENDER_EMAIL
+    msg['To'] = recipient_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(html_body, 'html'))
+
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(secrets.SENDER_EMAIL, secrets.SENDER_PASSWORD)
+
+        server.sendmail(secrets.SENDER_EMAIL, recipient_email, msg.as_string())
+        print('Email sent successfully')
+
+    except Exception as e:
+        print(f'Failed to send email: {e}')
+
+    finally:
+        # Close the connection to the SMTP server
+        server.quit()
+
+
+def send_email(recipient, message_html, subject):
+    # do not send emails while testing
+    if recipient != 'testtest@gmail.com' and recipient != 'test@example.com':
+        print('not sending email to ' + recipient)
+        return None
+
+    send_gmail_email(recipient, subject, message_html)
